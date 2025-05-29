66cf57714932d0c095e9d4bdbf797b3a084d4b7a
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-05-29 12:56:19 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,7 +131,7 @@ DJOSER = {
         'current_user': 'api.serializers.UserSerializer',
     },
     'PERMISSIONS': {
-        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],#ffffff
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     }
 }
