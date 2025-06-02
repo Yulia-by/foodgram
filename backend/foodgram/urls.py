27d7c36fa5052759b20e2b0cd7250a7a61bfdd24
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-06-02 14:00:59 +0300

    fix

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,7 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
