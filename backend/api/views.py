cf8fed86881306aa06fa0557225d2a695a2f6cd7
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-06-06 13:42:22 +0300

    fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import HttpResponse
 
-from rest_framework import status, viewsets, permissions
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
