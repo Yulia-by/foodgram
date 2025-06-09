d7cbc24de78bbd1146470152eb0382417cf42fa4
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-06-09 15:12:25 +0300

    fix

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from shortlink.views import load_url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('shortlink.urls')),
+    path('s/<str:url_hash>/', load_url, name='load_url'),
 ]
 
 if settings.DEBUG:
