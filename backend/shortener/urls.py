323a89a30beafdd54a924ddafa36038b59db6de7
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-06-10 14:00:36 +0300

    fix

--- backend/shortener/urls.py
+++ backend/shortener/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'shortlink'
+app_name = 'shortener'
 
 urlpatterns = [
     path('s/<str:url_hash>/', views.load_url, name='load_url'),
