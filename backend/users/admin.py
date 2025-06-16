4805491702d9b01f19306f8e718dc004ac868919
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-06-16 09:48:16 +0300

    fix

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
 
 from users.models import User
 
@@ -10,3 +12,6 @@ class UserAdmin(UserAdmin):
     list_display = ('id', 'first_name', 'last_name', 'email',)
     search_fields = ('email', 'first_name',)
     list_filter = ('email', 'first_name',)
+
+
+admin.site.unregister([Group, TokenProxy])
